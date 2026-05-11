1bd2e3f96af778aa3962fb0267dacb8f66397cf4
Anton Konyahin <me@konyahin.xyz>
2026-05-11 14:56:07 +0300

    feat: use first level header as a protocol name

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 import { renderHome } from "./templates/home.ts";
 import { renderProtocol } from "./templates/protocol.ts";
 import type { Protocol } from "./templates/types.ts";
-import { marked } from "marked";
+import { marked, type Tokens } from "marked";
 import { build } from "esbuild";
 
 const OUTPUT = "./output";
@@ -22,21 +22,29 @@ await build({
 });
 
 const protocolsFiles = await readdir(INPUT);
-
-const protocols: readonly Protocol[] = await Promise.all(protocolsFiles.map(async (file) => {
-    const title = path.parse(file).name;
-    const markdownContent = await readFile(path.join(INPUT, file), "utf-8")
-    const htmlContent = await marked.parse(markdownContent)
-    return { title, url: `${title}.html`, content: htmlContent };
-}));
+const protocols: readonly Protocol[] = await Promise.all(protocolsFiles.map(parseProtocol));
 
 await writeFile(path.join(OUTPUT, "index.html"), renderHome({ protocols }));
 
 for (const protocol of protocols) {
     await writeFile(
-        path.join(OUTPUT, `${protocol.title}.html`),
+        path.join(OUTPUT, `${protocol.url}`),
         renderProtocol(protocol),
     );
 }
 
 console.log(`Generated ${protocols.length + 1} pages in ${OUTPUT}/`);
+
+async function parseProtocol(file: string): Promise<Protocol> {
+    const fileName = path.parse(file).name;
+    const markdownContent = await readFile(path.join(INPUT, file), "utf-8")
+
+    const tokens = marked.lexer(markdownContent)
+    const h1 = tokens.find(
+        (t): t is Tokens.Heading => t.type === "heading" && t.depth === 1
+    );
+    const title = h1?.text ?? fileName;
+
+    const htmlContent = await marked.parse(markdownContent)
+    return { title: title, url: `${fileName}.html`, content: htmlContent };
+}
